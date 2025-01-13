3d2923d05163f1d9e286fc7c4de191c83951d80c
Danica <dt604@byu.edu>
2025-01-13 11:22:22 -0700

    initial setup based on the video

---
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -0,0 +1,12 @@
+package chess;
+
+// calculate piece movements -> https://byu.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=88653eac-78a8-4f59-a12a-b170014f61f1
+/*
+pieceMoves (board: ChessBoard, position: ChessPosition): Collection<ChessMove>
+*/
+public class PieceMovesCalculator  {
+    // create objects for KingMovesCalculator etc.
+    /*
+    pieceMoves (board: ChessBoard, position: ChessPosition): Collection<ChessMove>
+     */
+}
