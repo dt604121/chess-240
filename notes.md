8b556eb2b13a981900215dca34db6669afce3b60
Danica <dt604@byu.edu>
2025-01-15 08:27:37 -0700

    Chess Board Test is passing

--- notes.md
+++ notes.md
@@ -1,4 +1,15 @@
 ## Phase 0 
+~ Set up based on the video ~
+Commit #1 & #2 - ChessBoard.java / ChessPosition.java / ChessPiece.java
+1) ChessBoard.java - implement the constructor / data structure for the squares
+2) Implement addPiece/getPiece making sure to compensate for the fact that the board is 1-8 but in 
+CS it's 0-7 so we need to subtract one from the row and the column.
+3) Implement resetBoard by reinitializing the board to clear it and then setting up each of the chess
+pieces. Be sure to generate/include the overrides for toString, equals, and hash to make sure that the 
+object methods are taking into account row/col.
+4) ChessPiece.java - Create the PieceMovesCalculator. Have pieceMoves return an arrayList.
+5) ChessPosition.java - Initialize row/col as well as the getters/setters. Be sure to generate/include the overrides for toString, equals, and hash to make sure that the
+   object methods are taking into account row/col.
 
 ## Phase 1 
 
--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * Represents a single chess piece
@@ -10,9 +11,12 @@ import java.util.Collection;
  * signature of the existing methods.
  */
 public class ChessPiece {
+    private final ChessGame.TeamColor pieceColor;
+    private final ChessPiece.PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
-
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -31,14 +35,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -52,5 +56,30 @@ public class ChessPiece {
     // delegate to PieceMovesCalculator
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         return new ArrayList<>();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece that = (ChessPiece) o;
+        return pieceColor == that.pieceColor && type == that.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pieceColor, type);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
     }
 }
