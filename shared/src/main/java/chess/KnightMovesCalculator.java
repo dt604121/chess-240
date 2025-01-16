11281a17acf3630a437ee5d233a5f5b71b8791e8
Danica <dt604@byu.edu>
2025-01-16 12:57:09 -0700

    got started on rook -> isn't passing but should be closeish

--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -3,7 +3,6 @@ package chess;
 import java.util.ArrayList;
 import java.util.Collection;
 
-// TODO: implement the Knight!
 public class KnightMovesCalculator implements PieceMovesCalculator{
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
@@ -11,37 +10,49 @@ public class KnightMovesCalculator implements PieceMovesCalculator{
         // Find all combinations of the L..
         // move the row +/- 2
         // L -> row + 2 and col + 1
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, 2, 1);
         // L -> row + 2 and col - 1
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, 2, -1);
         // L -> row - 2 and col + 1
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, -2, 1);
         // L -> row - 2 and col - 1
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, -2, -1);
 
         // move the row +/- 1
         // L -> row + 1 and col + 2
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, 1, 2);
         // L -> row + 1 and col - 2
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, 1, -2);
         // L -> row - 1 and col - 2
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, -1, -2);
         // L -> row - 1 and col + 2
-
-        for (int row = myPosition.getRow(), col = myPosition.getColumn(); row < 9 && col < 9; row += 2, col += 1){
-            ChessPosition position = new ChessPosition(row, col);
-            ChessPiece originalPiece = board.getPiece(myPosition);
-            ChessPiece currentPiece = board.getPiece(myPosition);
-
-            if (currentPiece == null){
-                ChessMove validMove = new ChessMove(myPosition, position, null);
-                validMoves.add(validMove);
-            }
-            else{
-                // friendly fire?
-                if (currentPiece.getTeamColor() == originalPiece.getTeamColor()){
-                    break;
-                }
-                // capture enemy?
-                ChessMove validMove = new ChessMove(myPosition, position, null);
-                validMoves.add(validMove);
-                break;
-            }
-        }
+        PieceMovesCalculator.moveToEnd(board, myPosition, validMoves, -1, 2);
 
         return validMoves;
+    }
+    static void moveL(ChessBoard board, ChessPosition myPosition, ArrayList<ChessMove> validMoves,
+                          int rowDirection, int colDirection) {
+        // start the offset -> look ahead
+        int row = myPosition.getRow() + rowDirection;
+        int col = myPosition.getColumn() + colDirection;
+        if (row < 9 && row >= 1 && col < 9 && col >= 1) {
+            ChessPosition position = new ChessPosition(row, col);
+            ChessPiece originalPiece = board.getPiece(myPosition);
+            ChessPiece currentPiece = board.getPiece(position);
+            // open space?
+            if (currentPiece == null) {
+                ChessMove validMove = new ChessMove(myPosition, position, null);
+                validMoves.add(validMove);
+            }
+            // about to run into a friendly piece depending on color..
+            else {
+                if (currentPiece.getTeamColor() == originalPiece.getTeamColor()) {
+                    return;
+                }
+                // can capture an enemy
+                ChessMove validMove = new ChessMove(myPosition, position, null);
+                validMoves.add(validMove);
+            }
+        }
     }
 }
