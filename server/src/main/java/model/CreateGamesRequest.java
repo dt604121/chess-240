a82bb30099b53432c5983da416c7bea636c3151c
Danica <dt604@byu.edu>
2025-02-11 12:55:10 -0700

    Server + Response Exception

---
+++ server/src/main/java/model/CreateGamesRequest.java
@@ -0,0 +1,5 @@
+package model;
+
+public record CreateGamesRequest(
+        String gameName) {
+}
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,8 +1,16 @@
 package server;
 
+import com.google.gson.Gson;
+import model.*;
+import service.UserService;
 import spark.*;
 
 public class Server {
+    private final UserService userService;
+
+    public Server(UserService userService) {
+        this.userService = userService;
+    }
 
     public int run(int desiredPort) {
         Spark.port(desiredPort);
@@ -11,7 +19,15 @@ public class Server {
 
         // Register your endpoints and handle exceptions here.
 
-        //This line initializes the server and can be removed once you have a functioning endpoint 
+        Spark.post("/user", this::register);
+        Spark.post("/session", this::login);
+        Spark.delete("/session", this::logout);
+        Spark.get("/game", this::listGames);
+        Spark.post("/game", this::createGames);
+        Spark.put("/game", this::joinGame);
+        Spark.delete("/db", null);
+
+        // This line initializes the server and can be removed once you have a functioning endpoint
         Spark.init();
 
         Spark.awaitInitialization();
@@ -21,5 +37,41 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object register(Request req, Response res) throws ResponseException {
+        var registerRequest = new Gson().fromJson(req.body(), RegisterRequest.class);
+        var registerResult = userService.register(registerRequest);
+        return new Gson().toJson(registerResult);
+    }
+
+    private Object login(Request req, Response res) throws ResponseException {
+        var loginRequest = new Gson().fromJson(req.body(), LoginRequest.class);
+        var loginResult = userService.login(loginRequest);
+        return new Gson().toJson(loginResult);
+    }
+
+    private Object logout(Request req, Response res) throws ResponseException {
+        // var logoutRequest = new Gson().fromJson(req.body(), LogoutRequest.class);
+        // var logoutResult = userService.logout(logoutRequest);
+        return null;
+    }
+
+    private Object listGames(Request req, Response res) throws ResponseException {
+        var listGamesRequest = new Gson().fromJson(req.body(), ListGamesRequest.class);
+        var listGamesResult = userService.listGames(listGamesRequest);
+        return new Gson().toJson(listGamesResult);
+    }
+
+    private Object createGames(Request req, Response res) throws ResponseException {
+        var createGamesRequest = new Gson().fromJson(req.body(), CreateGamesRequest.class);
+        var createGamesResult = userService.listGames(createGamesRequest);
+        return new Gson().toJson(createGamesResult);
+    }
+
+    private Object joinGame(Request req, Response res) throws ResponseException{
+        var joinGamesRequest = new Gson().fromJson(req.body(), JoinGamesRequest.class);
+        var joinGamesResult = userService.listGames(joinGamesRequest);
+        return new Gson().toJson(joinGamesResult);
     }
 }
