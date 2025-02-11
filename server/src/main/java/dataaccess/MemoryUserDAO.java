5cc05fbefe414b566b31c6e790d1ab521b30930b
Danica <dt604@byu.edu>
2025-02-11 12:55:35 -0700

    Logout / login handlers

---
+++ server/src/main/java/dataaccess/MemoryUserDAO.java
@@ -0,0 +1,11 @@
+package dataaccess;
+
+import model.UserData;
+
+public class MemoryUserDAO implements UserDAO {
+    @Override
+    // TODO: implement
+    public UserData getUser(String username) {
+        return null;
+    }
+}
