c44a1e01c83a499aaab765b28751afe25694e7ee
Danica <dt604@byu.edu>
2025-02-28 18:16:31 -0700

    Unit Testing for the DAO's methods (base cases / skeletons)

--- server/src/test/java/dataaccess/DatabaseUnitTests.java
+++ server/src/test/java/dataaccess/DatabaseUnitTests.java
@@ -1,5 +1,6 @@
 package dataaccess;
 
+import chess.ChessGame;
 import dataaccess.sql.SQLUserDAO;
 import dataaccess.sql.SQLGameDAO;
 import dataaccess.sql.SQLAuthDAO;
@@ -12,6 +13,8 @@ import org.junit.jupiter.api.BeforeEach;
 
 import java.util.Collection;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class DatabaseUnitTests {
     SQLGameDAO sqlGameDAO = new SQLGameDAO();
     SQLUserDAO sqlUserDAO = new SQLUserDAO();
@@ -23,105 +26,201 @@ public class DatabaseUnitTests {
         sqlGameDAO.clearGameDAO();
         sqlAuthDAO.clearAuthDAO();
         sqlUserDAO.clearUserDAO();
-        // TODO: add psuedo data here for testing
-        // assert that it's not null as well...
     }
 
     //region Game DAO Tests
     @Test
     void listGamesPositiveTest() throws DataAccessException {
+        GameData gameData = new GameData(1234, "whiteUsername", "blackUsername",
+                "gameName", new ChessGame());
 
+        assertNotNull(sqlGameDAO.listGames(gameData));
     }
 
+    @Test
     void listGamesNegativeTest() throws DataAccessException {
+        // testing null input
+        GameData gameData = new GameData(0, null, null,
+                null, null);
 
+        assert(sqlGameDAO.listGames(gameData).isEmpty());
     }
 
+    @Test
     void getGamePositiveTest() throws DataAccessException {
-
+        GameData gameData = new GameData(1234, "whiteUsername", "blackUsername",
+                "gameName", new ChessGame());
+        assertNotNull(sqlGameDAO.getGame(1234));
     }
 
+    @Test
     void getGameNegativeTest() throws DataAccessException {
-
+        // mismatching gameID
+        GameData gameData = new GameData(1234, "whiteUsername", "blackUsername",
+                "gameName", new ChessGame());
+        assertNull(sqlGameDAO.getGame(123));
     }
 
+    @Test
     void clearGameDAOPositiveTest() throws DataAccessException {
+        GameData gameData = new GameData(1234, "whiteUsername", "blackUsername",
+                "gameName", new ChessGame());
 
+        sqlGameDAO.addGame(gameData);
+        sqlGameDAO.clearGameDAO();
+
+        assertNull(sqlGameDAO.getGame(1234), "GameDAO should be emtpy after clearing");
     }
 
+    @Test
     void updateGamePositiveTest() throws DataAccessException {
+        GameData gameData = new GameData(1234, "whiteUsername", "blackUsername",
+                "gameName", new ChessGame());
+        sqlGameDAO.updateGame(gameData);
 
+        assertEquals((gameData.gameID()), 1234);
     }
 
+    @Test
     void updateGameNegativeTest() throws DataAccessException {
+        // null data
+        GameData gameData = new GameData(0, null, null,
+                null, null);
 
+        sqlGameDAO.updateGame(gameData);
+
+        assertNotEquals((gameData.gameID()), 0);
     }
 
+    @Test
     void addGamePositiveTest() {
+        GameData gameData = new GameData(1234, "whiteUsername", "blackUsername",
+                "gameName", new ChessGame());
+        sqlGameDAO.addGame(gameData);
 
+        assertEquals((gameData.gameID()), 1234);
     }
 
+    @Test
     void addGameNegativeTest()  {
+        // null data
+        GameData gameData = new GameData(0, null, null,
+                null, null);
 
+        sqlGameDAO.addGame(gameData);
+
+        assertNotEquals((gameData.gameID()), 0);
     }
     // endregion
 
     // region User DAO Tests
+    @Test
     void getUserPositiveTest() throws DataAccessException {
+        UserData userData = new UserData("username", "password", "email.com");
 
+        assertNotNull(sqlUserDAO.getUser("username"));
+        assertEquals(userData.username(), "username");
     }
 
+    @Test
     void getUserNegativeTest() throws DataAccessException {
+        UserData userData = new UserData("username", "password", "email.com");
 
+        assertNull(sqlUserDAO.getUser("Danica"));
+        assertNotEquals(userData.username(), "Danica");
     }
 
+    @Test
     void createUserPositiveTest() throws DataAccessException {
-
+        sqlUserDAO.createUser("username", "password", "email.com");
+        assertNotNull(sqlUserDAO.getUser("username"));
     }
 
+    @Test
     void createUserNegativeTest() throws DataAccessException {
-
+        assertNull(sqlUserDAO.createUser(null, null, null));
     }
 
+    @Test
     void clearUserDAOPositiveTest() throws DataAccessException {
+        UserData userData = new UserData("clearUser", "clear", "clearUser@email");
+        sqlUserDAO.addUser(userData);
+        sqlUserDAO.clearUserDAO();
 
+        assertNull(sqlUserDAO.getUser("clearUser"), "UserDAO should be empty after clear.");
     }
 
+    @Test
     void addUserPositiveTest() throws DataAccessException {
+        UserData userData = new UserData("username", "password", "email.com");
 
+        sqlUserDAO.addUser(userData);
+        assertEquals(userData.username(), "username");
+        assertEquals(userData.password(), "password");
+        assertEquals(userData.email(), "email.com");
     }
 
+    @Test
     void addUserNegativeTest() throws DataAccessException {
+        UserData userData = new UserData(null, null, null);
 
+        sqlUserDAO.addUser(userData);
+        assertNull(userData.username(), userData.email());
     } // endregion
 
     // region Auth DAO Tests
+    @Test
     void getAuthTokenPositiveTest() throws DataAccessException {
-
+        AuthData authData = new AuthData("1234", "username");
+        sqlAuthDAO.addAuthToken(authData);
+        assertNotNull(sqlAuthDAO.getAuthToken("1234"));
     }
-
+    @Test
     void getAuthTokenNegativeTest() throws DataAccessException {
-
+        AuthData authData = new AuthData(null, "username");
+        sqlAuthDAO.addAuthToken(authData);
+        assertNotNull(sqlAuthDAO.getAuthToken(null));
     }
 
+    @Test
     void deleteAuthTokenPositiveTest() throws DataAccessException {
-
+        AuthData authData = new AuthData("1234", "username");
+        sqlAuthDAO.addAuthToken(authData);
+        sqlAuthDAO.deleteAuth("1234");
+        assertNull(sqlAuthDAO.getAuthToken("1234"));
     }
 
+    @Test
     void deleteAuthTokenNegativeTest() throws DataAccessException {
-
+        AuthData authData = new AuthData(null, "username");
+        sqlAuthDAO.addAuthToken(authData);
+        sqlAuthDAO.deleteAuth(null);
+        assertNull(sqlAuthDAO.getAuthToken(null));
     }
 
+    @Test
     void clearAuthDAOPositiveTest() throws DataAccessException {
+        AuthData authData = new AuthData("1234", "username");
+        sqlAuthDAO.addAuthToken(authData);
+        sqlAuthDAO.clearAuthDAO();
 
+        // throws error if the data in the DAO still exists
+        assertNull(sqlAuthDAO.getAuthToken("1234"),
+                "AuthDAO should be empty after clear.");
     }
 
+    @Test
     void addAuthTokenPositiveTest() throws DataAccessException {
-
+        AuthData authData = new AuthData("1234", "username");
+        sqlAuthDAO.addAuthToken(authData);
+        assertEquals(authData.authToken(), "1234");
     }
 
+    @Test
     void addAuthTokenNegativeTest() throws DataAccessException {
-
+        AuthData authData = new AuthData(null, "username");
+        sqlAuthDAO.addAuthToken(authData);
+        assertNull(authData.authToken());
     }
     // endregion
 }
